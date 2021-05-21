028f8d54327082693a390b70be727278010190d1
Markus Blechschmidt <blechschmidt@simplexion.de>
2021-05-21 13:56:47 +0200

    renamed package

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ with open(path.join(here, "README.rst"), encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
-    name="adafruit-circuitpython-mcp4728",
+    name="adafruit-pureio-mcp4728",
     # use_scm_version={"root": ".", "relative_to": __file__},
     use_scm_version=True,
     setup_requires=["setuptools_scm"],
